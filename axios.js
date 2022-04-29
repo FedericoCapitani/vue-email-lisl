39cd7376011cc294745ef20b2b8773fb7780d323
FedericoCapitani <federico.capitani@outlook.it>
2022-04-29 14:48:14 +0200

    creare 10 email

--- axios.js
+++ axios.js
@@ -2,13 +2,18 @@ const app = new Vue({
     el: '#app',
     data: {
         mail: '',
-        my_data: null
+        my_data: null,
+        my_emails: []
     },
     methods: {},
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            console.log(this);
-            this.my_data = response.data
-        })
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                console.log(this);
+                this.my_data = response.data
+                this.my_emails.push(response.data)
+            })
+            
+        }
     }
 })
