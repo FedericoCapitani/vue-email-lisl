48ed20d5f2f364262ae5709f739bea4216f193f3
FedericoCapitani <federico.capitani@outlook.it>
2022-04-29 13:37:23 +0200

    crea email

--- axios.js
+++ axios.js
@@ -5,5 +5,10 @@ const app = new Vue({
         my_data: null
     },
     methods: {},
-    
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+            console.log(this);
+            this.my_data = response.data
+        })
+    }
 })
